3dfa824f61840e9c844c1476ba7764d988f52366
Urvashi Parmar <parmarurvashi01@gmail.com>
2023-08-21 10:26:47 +0530

    feat: Item list cards on Home Page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+  const [itemList, setItemList] = React.useState([]);
+  let pageNo = 1;
 
-export default HomePage
+  React.useEffect(() => {
+    fetch(`${import.meta.env.VITE_BASE_URL}/api/v1/items?page=${pageNo}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setItemList(data.data);
+      });
+  }, []);
+
+  return (
+    <div className="item-list">
+      {itemList.length > 0 &&
+        itemList.map((item, key) => <ItemCard key={item.id} {...item} />)}
+    </div>
+  );
+};
+
+const ItemCard = ({
+  id,
+  title,
+  imgList,
+  listType,
+  location,
+  price,
+  createdAt,
+}) => {
+  return (
+    <div className="item-card-container">
+      <div className="item-card">
+        <div className="item-card-imgs">
+          {
+            // <img src={imgList[0]} alt="" />
+            imgList.length > 0 ? (
+              imgList.map((img) => <img src={img} />)
+            ) : (
+              <div>NO Image</div>
+            )
+          }
+        </div>
+        <div className="img-card-body">
+          <div className="img-card-title">{title}</div>
+          <div className="img-card-row-space-between">
+            <div className="img-card-location">{location}</div>
+            <div className="img-card-price">{price}</div>
+          </div>
+        </div>
+        <div className="img-card-footer">
+          <div>{listType}</div>
+          <div>{createdAt}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
